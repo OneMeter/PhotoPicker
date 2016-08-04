abc56cf28cc45eddbda8bdf314738539fcc1e732
杜岳 <593761880@qq.com>
2016-08-04 17:55:52 +0800

    2016.8.4

---
+++ PhotoPicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoPicker
+//
+//  Created by htkg on 16/8/4.
+//  Copyright © 2016年 Vincent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
